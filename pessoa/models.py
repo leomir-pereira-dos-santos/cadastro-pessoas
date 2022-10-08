2df964202f462cf86455ba63ffb3e96f380d470e
leomir-pereira-dos-santos <leomir101@gmail.com>
2022-10-08 20:26:05 -0300

    criado html,e novas paginas,contatos

--- pessoa/models.py
+++ pessoa/models.py
@@ -4,3 +4,15 @@ class Pessoa(models.Model):
     nome_completo = models.CharField(max_length=256)
     data_nascimento = models.DateField(null=True) 
     ativa = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.nome_completo
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=256)
+    email = models.EmailField(max_length=256)
+    telefone = models.CharField(max_length=20)
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.nome
