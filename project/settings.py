b983d14c03b470f49a58622418f49464367bc9f7
leomir-pereira-dos-santos <leomir101@gmail.com>
2022-10-11 11:44:40 -0300

    alterado host

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['cadastro-de-pessoa.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
